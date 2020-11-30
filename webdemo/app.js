9bba8911409e5b4491ceda05c0073fab7fd300a6
Susan Trinh <snt.trinh@gmail.com>
2020-11-30 16:25:32 -0700

    Rendered images

--- webdemo/app.js
+++ webdemo/app.js
@@ -3,12 +3,15 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-//const fs = require("fs");
+// const fs = require("fs");
 
 const PORT = 8000;
 
-app.use("/static", express.static(__dirname));
-app.use(express.static("static", {extensions: ["jpg", "png"]}));
+// app.use("/static", express.static(path.join(__dirname, "static")));
+app.use(express.static("static", {extensions: ["html", "htm"]}));
+
+
+app.use(express.static("static/media", {extensions: ["jpg", "png", "svg"]}));
 
 app.get("/", (req, res)=>{
     res.sendFile(__dirname +"/static/index.html");
