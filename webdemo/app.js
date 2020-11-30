23d4773ef653be529a393d71d97e2c0ac36d96bc
Susan Trinh <snt.trinh@gmail.com>
2020-11-30 16:14:18 -0700

    Loaded images on index page

--- webdemo/app.js
+++ webdemo/app.js
@@ -1,9 +1,18 @@
 
 const express = require("express");
+const path = require("path");
 const app = express();
+
 //const fs = require("fs");
 
 const PORT = 8000;
+
+app.use("/static", express.static(__dirname));
+app.use(express.static("static", {extensions: ["jpg", "png"]}));
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname +"/static/index.html");
+});
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
--- webdemo/js/indexLoader.js
+++
@@ -1,8 +0,0 @@
-// Loader function - once everything above has loaded, this function calls a 3 second timer then adds load class to body, triggering animations 
-setTimeout(function(){
-    const body = document.getElementsByTagName('body')[0];
-        body.classList.add("loaded");
-}, 3000)
-setTimeout(function(){ //A second timer brings the window back to the top just before the page reveals, to counter random scroll flailing
-    window.scrollTo(0,0);
-},8400);
