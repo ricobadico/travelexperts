e010b7ae7f76244501118d48fa5df0623d06e187
ricobadico <biondiceric@gmail.com>
2020-11-30 17:05:23 -0700

    testgin stuff

--- webdemo/app.js
+++ webdemo/app.js
@@ -1,7 +1,15 @@
 
 const express = require("express");
 const path = require("path");
+const handlebars = require("express-handlebars");
+
 const app = express();
+
+app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars({
+    extname: "handlebars",
+    defaultLayout: ""
+}))
 
 // const fs = require("fs");
 
@@ -16,6 +24,10 @@ app.use(express.static("static/media", {extensions: ["jpg", "png", "svg"]}));
 app.get("/", (req, res)=>{
     res.sendFile(__dirname +"/static/index.html");
 });
+
+app.get("/handlebars", (req,res) => {
+    res.render(__dirname + "/static/contact.handlebars", { handlebartitle: "WELCOME TO QUEENS GAMBIT CITY", detail: "GIVE US A CALL", tidbit: ["hello", "what's up"] })
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
