494b53a7faeb6e3b488b24c1da57320ca8a3da3c
ricobadico <biondiceric@gmail.com>
2020-11-30 19:43:56 -0700

    got images working, added handlebars with example

--- webdemo/app.js
+++ webdemo/app.js
@@ -6,6 +6,7 @@ const handlebars = require("express-handlebars");
 const app = express();
 
 app.set('view engine', 'handlebars');
+// Handlebars needs this engine line to configure it and work properly
 app.engine('handlebars', handlebars({
     extname: "handlebars",
     defaultLayout: ""
@@ -16,7 +17,7 @@ app.engine('handlebars', handlebars({
 const PORT = 8000;
 
 // app.use("/static", express.static(path.join(__dirname, "static")));
-app.use(express.static("static", {extensions: ["html", "htm"]}));
+app.use(express.static("static", {extensions: ["html", "htm", "css", "js"]}));
 
 
 app.use(express.static("static/media", {extensions: ["jpg", "png", "svg"]}));
@@ -25,8 +26,9 @@ app.get("/", (req, res)=>{
     res.sendFile(__dirname +"/static/index.html");
 });
 
+// Learning example; this and contact.handlebars can be deleted
 app.get("/handlebars", (req,res) => {
-    res.render(__dirname + "/static/contact.handlebars", { handlebartitle: "WELCOME TO QUEENS GAMBIT CITY", detail: "GIVE US A CALL", tidbit: ["hello", "what's up"] })
+    res.render(__dirname + "/views/contact.handlebars", { handlebartitle: "WELCOME TO QUEENS GAMBIT CITY", detail: "GIVE US A CALL", tidbit: ["hello", "what's up"] })
 })
 
 app.listen(PORT, ()=>{
