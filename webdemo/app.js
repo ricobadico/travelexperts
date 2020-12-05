d397ad65eb6115f2ab5e59d3cc2084145bbcf15a
Bobby Boyd <jeffbob@gmail.com>
2020-12-05 01:27:44 -0700

    -added start of login form to navbar -fixed the sql query for reg page 'no default value on CustBusPhone -just passed the regular number to it -logged out connection error correctly and closed conn on error -set up reg conn to hold on return value of query and log it out -holds object that contains the insertId of the inserted record

--- webdemo/app.js
+++ webdemo/app.js
@@ -5,11 +5,26 @@ const mysql = require("mysql");
 const dotenv = require("dotenv");
 
 //Load Config
-dotenv.config({ path: "./.env", debug: true });
+dotenv.config({ path: "./.env", debug: false });
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = process.env.PORT || 8000;
-//const PORT = 8000;
+
+// Open up the database for use. Any express method call can use this 'connection' variable, but needs to connect and end it's particular connection instance! (see existing examples)
+// prettier-ignore
+const getConnection = () => {
+
+    let connection = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      multipleStatements: true
+    });
+    return connection;
+}
 
 //  Set handlebars as view engine
 app.set("view engine", "handlebars");
@@ -20,32 +35,14 @@ app.engine("handlebars", handlebars({extname: "handlebars"}));
 // Set up serving of static files
 //app.use("/static", express.static(path.join(__dirname, "public")));
 app.use("/", express.static(path.join(__dirname, "static")));
-/*
-app.use(
-  "/",
-  express.static(path.join(__dirname, "static"), {
-    extensions: ["html", "htm", "css", "js"],
-  })
-);
 
-app.use(
-  "/",
-  express.static(path.join(__dirname, "static/media"), {
-    extensions: ["jpg", "png", "svg"],
-  })
-);
-*/
 app.listen(PORT, () => {
   console.log(
     `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
   );
 });
 
-// Serves up index.html on the root
-//app.get("/index", (req, res) => {
-//  res.sendFile(__dirname + "/static/index.html");
-//});
-
+// Define our routes and render templates
 app.get("/register", (req, res) => {
   console.log("render register");
   res.render("register");
@@ -55,6 +52,7 @@ app.get("/packages", (req, res) => {
   console.log("render packages");
   res.render("packages");
 });
+
 app.get("/", (req, res) => {
   console.log("render home");
   res.render("home");
@@ -63,17 +61,7 @@ app.get("/", (req, res) => {
 // Feisty template render for Contact page, requires nested queries fed into a complicated template
 // It works but occasionally fails to pull from the db, I'll work on it
 app.get("/contact", (req, res) => {
-  // prettier-ignore
-  // Open up the database for use.
-  const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: "root",
-    password: "password",
-    database: "travelexperts",
-    multipleStatements: true,
-    insecureAuth: true
-  });
-
+  let connection = getConnection();
   connection.connect();
 
   //Start building the array that will contain the data we need, in a form useful to handlebars
@@ -107,10 +95,65 @@ app.get("/contact", (req, res) => {
       dynamicAgencyList.Agencies[homeAgency - 1].agents.push(agents[j]);
     }
     //console.log(agents);
-
+    console.log("render contacts");
     // We now have all the data needed to populate the template, in the form the template is expecting
     // prettier-ignore
-    res.render(path.join(__dirname, "/views/contacts2.handlebars"), dynamicAgencyList);
+    res.render("contacts2", dynamicAgencyList);
     connection.end();
   });
+});
+
+// Insert the Register data into the database. All the Register page form needs to do is have "registerPOST" as its action to fire this off
+app.post("/registerPOST", (req, res) => {
+  // This is just fancy "javascript destructuring": assigns these variables to the corresponding properties of the req.body object
+  let {
+    firstName,
+    lastName,
+    userName,
+    pwd,
+    email,
+    pNumber,
+    address,
+    city,
+    prov,
+    pCode,
+    sendInfo,
+  } = req.body;
+  console.log(req.body);
+  // Use the defined connection config to connect
+  let connection = getConnection();
+  connection.connect();
+
+  // Make a query based off of register form data, with placeholders for body entries
+  // We'll write the sql as a variable on its own line to break up the code a little.
+  //You could instead write it as a string directly as the connection.query first argument
+  //Note this query looks like lot - the format really just mean:
+  //INSERT A NEW ENTRY INTO [a database table] (data,for,this,list,of,columns) THESE VALUE (one,datum,for,each,column,respectively)
+  let sql =
+    "INSERT INTO customers (`CustFirstName`, `CustLastName`, `CustEmail`, `CustHomePhone`, `CustAddress`, `CustCity`, `CustProv`, `CustPostal` , `CustBusPhone`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+  // Use a mysql connection's built-in query function to query the database. First argument is the SQL query, second argument defines placeholders ('?') in that query.
+  // Third argument is the callback that happens once you've successfully gotten back the data (or an error) from the database
+  let result = connection.query(
+    sql,
+    [firstName, lastName, email, pNumber, address, city, prov, pCode, pNumber],
+    function (error, result) {
+      if (error) {
+        connection.end();
+        console.log(error);
+        throw Error;
+      }
+
+      // We're going to leave a console.log here just so anyone on the server can confirm that something happened
+      console.log(result);
+
+      // We've done what we needed and can close the mysql connection!
+      connection.end();
+
+      // The user sent a request "/registerPOST" and is expecting a response! You need to tell the response to do something before we finish this express method call.
+      // Right now we go to the index page, but a page that acknowledges that they've been registered would be better.
+      console.log("returning home after register post");
+      res.render("home");
+    }
+  );
 });
--- webdemo/static/js/indexLoader.js
+++ webdemo/static/js/indexLoader.js
@@ -1,10 +1,19 @@
-// Loader function - once everything above has loaded, this function calls a 3 second timer then adds load class to body, triggering animations
-setTimeout(function () {
-  const body = document.getElementsByTagName("body")[0];
-  body.classList.add("loaded");
-}, 3000);
-setTimeout(function () {
-  //A second timer brings the window back to the top just before the page reveals, to counter random scroll flailing
-  window.scrollTo(0, 0);
-}, 8400);
+// create a place to hold global vars
+if (!window.webapp) {
+  window.webapp = {};
+  webapp.introAnimation = true;
+}
 
+if (webapp.introAnimation) {
+  // Loader function - once everything above has loaded, this function calls a 3 second timer then adds load class to body, triggering animations
+  setTimeout(function () {
+    const body = document.getElementsByTagName("body")[0];
+    body.classList.add("loaded");
+  }, 3000);
+  setTimeout(function () {
+    //A second timer brings the window back to the top just before the page reveals, to counter random scroll flailing
+    window.scrollTo(0, 0);
+  }, 8400);
+  // set to false after run once
+  webapp.introAnimation = false;
+}
